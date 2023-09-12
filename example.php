5945c1bd47f1b9809d8c990d4667224e2f83bb9c
goerdy <github@philipp-guerth.de>
2023-09-12 23:24:24 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,6 +1,15 @@
 <?php
 require('mkCal.php');
 
+$DaysTextArray;
+$DaysColorArray;
+$DaysHollidaysArray;
+
+$DaysTextArray[2][18]="Philipps Birthday";
+$DaysColorArray[2][18]="green";
+$DaysHollydaysArray[1][1]="New Year";
+$DaysHollydaysArray[3][17]="true";
+
 function mkCal($title, $Year, $locale, $daysText, $daysColor, $daysHollidays, $highlightHollidays, $highlightSunday, $ighlightSaturday, $footer, $format)
 mkCal("Test Calendar", 2023 ,"de_DE", daystext, dayscolor, dayshollidays, "magenta" ,"red", "firebrick1", "some idiotic footer text", "A4Landscape");
 
