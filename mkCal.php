3c2776ad03fba27785540c92b5c30d55450254be
goerdy <github@philipp-guerth.de>
2023-09-12 23:34:30 +0200

    Update mkCal.php

--- mkCal.php
+++ mkCal.php
@@ -46,7 +46,7 @@ function mkCal($title, $Year, $locale, $daysText, $daysColor, $daysHollidays, $h
     $w = 22; //Cell Width
     $this->Cell(8,7,' ',1,0,'C',true);
     for($i=1;$i<=12;$i++)
-        $this->Cell($w,7,$date = strftime('%B',strtotime($i."/01/1970"));  ,1,0,'C',true);
+        $this->Cell($w,7,$date = strftime('%B',strtotime($i."/01/1970")) ,1,0,'C',true);
     $this->Ln();
     // Color and font restoration
     $this->SetFillColor(224,235,255);
