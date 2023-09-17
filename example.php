b102511cfb1f0dccf5b5951e1e38f3bee697708a
goerdy <github@philipp-guerth.de>
2023-09-17 08:48:28 +0200

    Update example.php

--- example.php
+++ example.php
@@ -5,12 +5,14 @@ $DaysTextArray;
 $DaysColorArray;
 $DaysHollidaysArray;
 
-$DaysTextArray[2][18]="Philipps Birthday";
-$DaysColorArray[2][18]="green";
+$DaysTextArray[2][18]="goerdy";
+$DaysColorArray[2][18]="#FF0000";
+$DaysColorArray[4][22]="#00FF00";
+$DaysColorArray[12][3]="#0000FF";
 $DaysHollydaysArray[3][17]="true";
 
-//function mkCal($title, $Year, $locale, $daysText, $daysColor, $daysHollidays, $highlightHollidays, $highlightSunday, $ighlightSaturday, $footer, $format)
+//function mkCal($title, $Year, $locale, $daysText, $daysColor, $daysHollidays, $highlightHollidays, $highlightSunday, $ighlightSaturday, $footer, $format, $colorSceme)
 $pdf = new PDF('L','mm','A4');
-$pdf->mkCal("Test Calendar", 2023 ,"de_DE", $DaysTextArray, $DaysColorArray, $DaysHollydaysArray, "magenta" ,"red", "firebrick1", "some idiotic footer text", "A4Landscape");
+$pdf->mkCal("Example Calendar", 2024 ,"en_US", $DaysTextArray, $DaysColorArray, $DaysHollydaysArray, "#AAAA00" ,"#FF0000", "#990000", "some customizeable footer text", "A4Landscape", "#0000FF");
 
 ?>
